1e7f9ba924ec93c268e8da8959f8cdcb89aa418e
Tushar-W <twankhede95@gmail.com>
2020-05-18 10:26:54 +0530

    ADDED:code to create functionality for check two feets are equal or not

---
+++ src/main/java/com/bl/service/QuantityMeasurement.java
@@ -0,0 +1,9 @@
+package com.bl.service;
+
+public class QuantityMeasurement {
+    public boolean checkIsEqualFeets(int feet1, int feet2) {
+        if(feet1 == feet2)
+            return true;
+        return false;
+    }
+}
