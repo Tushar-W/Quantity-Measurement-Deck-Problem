0434500800382560ebba4fa7c1ae0b68d3ffa8d7
Tushar-W <twankhede95@gmail.com>
2020-05-21 10:59:52 +0530

    ADDED:code to create custom exception

---
+++ src/main/java/com/bl/exception/QuantityMeasurementException.java
@@ -0,0 +1,23 @@
+package com.bl.exception;
+
+public class QuantityMeasurementException extends RuntimeException {
+    /**
+     * object of enum class ExceptionType
+     */
+    public ExceptionType type;
+    /**
+     * enum for take constants Exception Types
+     */
+    public enum ExceptionType{
+        NULL_VALUE
+    }
+    /**
+     * constructor to take two input values
+     * @param type
+     * @param message
+     */
+    public QuantityMeasurementException(ExceptionType type, String message) {
+        super(message);
+        this.type = type;
+    }
+}
