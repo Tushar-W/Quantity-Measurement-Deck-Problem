72c07a4518e17253e024483af3862cedd6306eb6
Tushar-W <twankhede95@gmail.com>
2020-05-21 10:56:35 +0530

    ADDED:code to create functionality for feets are null

--- src/main/java/com/bl/model/Feet.java
+++ src/main/java/com/bl/model/Feet.java
@@ -5,20 +5,24 @@
  *******************************************************************************************/
 package com.bl.model;
 
+import com.bl.exception.QuantityMeasurementException;
+
 public class Feet {
     /**
      * value variable store the number entered by the user as the input
      */
     private final double value;
-
     /**
      * Parameterized constructor to take one input value
      * @param value
      */
-    public Feet(double value) {
-        this.value = value;
+    public Feet(Double value) throws QuantityMeasurementException {
+        try{
+            this.value = value;
+        }catch (NullPointerException e) {
+                throw new QuantityMeasurementException(QuantityMeasurementException.ExceptionType.NULL_VALUE, "VALUES IS NULL");
+        }
     }
-
     /**
      * This method compares the equality of the current object with the object of same type
      * @param obj
