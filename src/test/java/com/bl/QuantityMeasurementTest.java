6d050a60a5eb590c88d4de37db78c30e514dea94
Tushar-W <twankhede95@gmail.com>
2020-05-21 09:50:24 +0530

    ADDED:tested method to check when two feets are not equals

--- src/test/java/com/bl/QuantityMeasurementTest.java
+++ src/test/java/com/bl/QuantityMeasurementTest.java
@@ -1,5 +1,6 @@
 package com.bl;
 
+import com.bl.model.Feet;
 import com.bl.service.QuantityMeasurement;
 import org.junit.Assert;
 import org.junit.Test;
@@ -7,11 +8,18 @@ import org.junit.Test;
 public class QuantityMeasurementTest {
 
     @Test
-    public void givenTwoFeets_WhenBothFeetAreZero_ShouldReturnEqual() {
-        QuantityMeasurement quantityMeasurement = new QuantityMeasurement();
-        int feet1 = 0;
-        int feet2 = 0;
-        boolean isEqualFeets = quantityMeasurement.checkIsEqualFeets(feet1, feet2);
-        Assert.assertTrue(isEqualFeets);
+    public void givenTwoFeets_WhenBothFeetAreEquals_ShouldReturnTrue() {
+        Feet feet1 = new Feet(0.0);
+        Feet feet2 = new Feet(0.0);
+        boolean equals = feet1.equals(feet2);
+        Assert.assertTrue(equals);
+    }
+
+    @Test
+    public void givenTwoFeets_WhenBothFeetAreNotEquals_ShouldReturnFalse() {
+        Feet feet1 = new Feet(0.0);
+        Feet feet2 = new Feet(2.0);
+        boolean equals = feet1.equals(feet2);
+        Assert.assertFalse(equals);
     }
 }
