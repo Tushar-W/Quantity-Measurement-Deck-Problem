992eb6de49a192a21748109f3b0b450883390ce3
Tushar-W <twankhede95@gmail.com>
2020-05-21 17:56:54 +0530

    ADDED:tested to check equality of references

--- src/test/java/com/bl/QuantityMeasurementTest.java
+++ src/test/java/com/bl/QuantityMeasurementTest.java
@@ -9,27 +9,33 @@ public class QuantityMeasurementTest {
 
     @Test
     public void givenTwoFeets_WhenBothFeetAreEquals_ShouldReturnTrue() throws QuantityMeasurementException {
-        Feet feet1 = new Feet(0.0);
-        Feet feet2 = new Feet(0.0);
+        Feet feet1 = new Feet(0);
+        Feet feet2 = new Feet(0);
         boolean equals = feet1.equals(feet2);
         Assert.assertTrue(equals);
     }
 
     @Test
     public void givenTwoFeets_WhenBothFeetAreNotEquals_ShouldReturnFalse() throws QuantityMeasurementException {
-        Feet feet1 = new Feet(0.0);
-        Feet feet2 = new Feet(2.0);
+        Feet feet1 = new Feet(0);
+        Feet feet2 = new Feet(2);
         boolean equals = feet1.equals(feet2);
         Assert.assertFalse(equals);
     }
 
     @Test
-    public void givenFeets_WhenFeetsAreNull_ShouldReturnFalse() {
+    public void givenFeets_WhenFeetsAreNull_ShouldReturnThrowException() {
         try {
             Feet feet1 = new Feet(null);
             Feet feet2 = new Feet(null);
         }catch (QuantityMeasurementException e){
             Assert.assertEquals(QuantityMeasurementException.ExceptionType.NULL_VALUE, e.type);
         }
+    }
+
+    @Test
+    public void givenFeets_WhenReferenceOfFeetsAreEqual_ShouldReturnTrue() {
+        Feet feet1 = new Feet(2);
+        Assert.assertTrue(feet1.equals(feet1));
     }
 }
