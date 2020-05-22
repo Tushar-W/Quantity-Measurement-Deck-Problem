0cdad16ba7b05d1f571a219128ba5861b140fc15
Tushar-W <twankhede95@gmail.com>
2020-05-22 14:48:47 +0530

    REFACTORED:test method to pass values with units Feet or Inch

--- src/test/java/com/bl/QuantityMeasurementTest.java
+++ src/test/java/com/bl/QuantityMeasurementTest.java
@@ -1,8 +1,7 @@
-package com.bl;
+ package com.bl;
 
 import com.bl.exception.QuantityMeasurementException;
-import com.bl.model.Feet;
-import com.bl.model.Inch;
+import com.bl.model.Length;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -10,25 +9,36 @@ public class QuantityMeasurementTest {
 
     @Test
     public void givenTwoFeets_WhenBothFeetAreEquals_ShouldReturnTrue() {
-        Feet feet1 = new Feet(0);
-        Feet feet2 = new Feet(0);
+        Length feet1 = new Length(Length.Units.FEET, 0);
+        Length feet2 = new Length(Length.Units.FEET, 0);
         boolean equals = feet1.equals(feet2);
         Assert.assertTrue(equals);
     }
 
     @Test
     public void givenTwoFeets_WhenBothFeetAreNotEquals_ShouldReturnFalse() {
-        Feet feet1 = new Feet(0);
-        Feet feet2 = new Feet(2);
+        Length feet1 = new Length(Length.Units.FEET, 0);
+        Length feet2 = new Length(Length.Units.FEET, 2);
         boolean equals = feet1.equals(feet2);
         Assert.assertFalse(equals);
     }
 
     @Test
+    public void givenTwoFeets_WhenFeetAreNegative_ShouldReturnThrowException() {
+        try{
+            Length feet1 = new Length(Length.Units.FEET, -1);
+            Length feet2 = new Length(Length.Units.FEET, -2);
+            boolean equals = feet1.equals(feet2);
+        }catch (QuantityMeasurementException e){
+            Assert.assertEquals(QuantityMeasurementException.ExceptionType.NEGATIVE_VALUE, e.type);
+        }
+    }
+
+    @Test
     public void givenFeets_WhenFeetsAreNull_ShouldReturnThrowException() {
         try {
-            Feet feet1 = new Feet(null);
-            Feet feet2 = new Feet(null);
+            Length feet1 = new Length(Length.Units.FEET, null);
+            Length feet2 = new Length(Length.Units.FEET, null);
         }catch (QuantityMeasurementException e){
             Assert.assertEquals(QuantityMeasurementException.ExceptionType.NULL_VALUE, e.type);
         }
@@ -36,43 +46,54 @@ public class QuantityMeasurementTest {
 
     @Test
     public void givenFeets_WhenReferenceOfFeetsAreEqual_ShouldReturnTrue() {
-        Feet feet1 = new Feet(2);
+        Length feet1 = new Length(Length.Units.FEET, 2);
         Assert.assertTrue(feet1.equals(feet1));
     }
 
     @Test
     public void givenFeets_WhenTypeOfFeetsAreEqual_ShouldReturnTrue() {
-        Feet feet1 = new Feet(0);
-        Feet feet2 = new Feet(0);
+        Length feet1 = new Length(Length.Units.FEET, 0);
+        Length feet2 = new Length(Length.Units.FEET, 0);
         Assert.assertTrue(feet1.equals(feet2));
     }
 
     @Test
     public void givenFeets_WhenValueOfObjectAreEqual_ShouldReturnTrue() {
-        Feet feet1 = new Feet(3);
-        Feet feet2 = new Feet(3);
+        Length feet1 = new Length(Length.Units.FEET, 3);
+        Length feet2 = new Length(Length.Units.FEET, 3);
         Assert.assertTrue(feet1.equals(feet2));
     }
 
     @Test
     public void givenTwoInch_WhenBothInchAreEquals_ShouldReturnTrue() {
-        Inch inch = new Inch(0);
-        Inch inch2 = new Inch(0);
+        Length inch = new Length(Length.Units.INCH, 0);
+        Length inch2 = new Length(Length.Units.INCH, 0);
         Assert.assertTrue(inch.equals(inch2));
     }
 
     @Test
     public void givenInches_WhenBothInchesAreNotEquals_ShouldReturnFalse() {
-        Inch inch = new Inch(0);
-        Inch inch2 = new Inch(2);
+        Length inch = new Length(Length.Units.INCH, 0);
+        Length inch2 = new Length(Length.Units.INCH, 2);
         Assert.assertFalse(inch.equals(inch2));
+    }
+
+    @Test
+    public void givenTwoInches_WhenInchesAreNegative_ShouldReturnThrowException() {
+        try{
+            Length inch1 = new Length(Length.Units.INCH, -1);
+            Length inch2 = new Length(Length.Units.INCH, -2);
+            boolean equals = inch1.equals(inch2);
+        }catch (QuantityMeasurementException e){
+            Assert.assertEquals(QuantityMeasurementException.ExceptionType.NEGATIVE_VALUE, e.type);
+        }
     }
 
     @Test
     public void givenInches_WhenInchesAreNull_ShouldReturnThrowException() {
         try {
-            Inch inch = new Inch(null);
-            Inch inch2 = new Inch(null);
+            Length inch = new Length(Length.Units.INCH, null);
+            Length inch2 = new Length(Length.Units.INCH, null);
         }catch (QuantityMeasurementException e){
             Assert.assertEquals(QuantityMeasurementException.ExceptionType.NULL_VALUE, e.type);
         }
@@ -80,22 +101,29 @@ public class QuantityMeasurementTest {
 
     @Test
     public void givenInches_WhenReferenceOfInchesAreEqual_ShouldReturnTrue() {
-        Inch inch = new Inch(2);
+        Length inch = new Length(Length.Units.INCH, 2);
         Assert.assertTrue(inch.equals(inch));
     }
 
     @Test
     public void givenInches_WhenTypeOfInchesAreEqual_ShouldReturnTrue() {
-        Inch inch = new Inch(0);
-        Inch inch2 = new Inch(0);
+        Length inch = new Length(Length.Units.INCH, 0);
+        Length inch2 = new Length(Length.Units.INCH, 0);
         Assert.assertTrue(inch.equals(inch2));
     }
 
     @Test
     public void givenInches_WhenValueOfObjectAreEqual_ShouldReturnTrue() {
-        Inch inch = new Inch(2);
-        Inch inch2 = new Inch(2);
+        Length inch = new Length(Length.Units.INCH, 2);
+        Length inch2 = new Length(Length.Units.INCH, 2);
         Assert.assertTrue(inch.equals(inch2));
     }
 
+    @Test
+    public void givenFeetAndInch_WhenBothAreEquals_ShouldReturnTrue() {
+        Length feet1 = new Length(Length.Units.FEET, 1);
+        Length inch1 = new Length(Length.Units.INCH, 1);
+        boolean equals = feet1.equals(inch1);
+        Assert.assertFalse(equals);
+    }
 }
