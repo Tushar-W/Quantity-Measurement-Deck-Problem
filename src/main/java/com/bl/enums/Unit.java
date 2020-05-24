e9d8b777d57c6faf5cfcf1117325b93acd124fac
Tushar-W <twankhede95@gmail.com>
2020-05-25 01:24:56 +0530

    ADDED:code to create functionality for check equality of unit litres

--- src/main/java/com/bl/enums/Unit.java
+++ src/main/java/com/bl/enums/Unit.java
@@ -1,12 +1,13 @@
 package com.bl.enums;
 
 import com.bl.model.Length;
+import com.bl.model.Volume;
 
 public enum Unit {
     /**
      * declaring enum constatnts
      */
-    FEET(12.0), INCH(1.0), YARD(36.0), CENTIMETER(0.4);
+    FEET(12.0), INCH(1.0), YARD(36.0), CENTIMETER(0.4), LITRE(1);
     /**
      * final constant variable
      */
@@ -23,11 +24,23 @@ public enum Unit {
      * @param l1, l2
      * @return true or false
      */
-    public boolean compare(Length l1, Length l2){
+    public boolean compareLength(Length l1, Length l2){
         return  Double.compare(l1.value*l1.unit.baseUnitConversion, l2.value*l2.unit.baseUnitConversion) == 0;
     }
-
+    /**
+     * This method add unit length of the current object with object of same or different type
+     * @param l1, l2
+     * @return addition
+     */
     public double add(Length l1, Length l2) {
         return Double.sum(l1.value*l1.unit.baseUnitConversion, l2.value*l2.unit.baseUnitConversion);
+    }
+    /**
+     * This method comapre unit volume of the current object with object of same or different type
+     * @param v1, v2
+     * @return true or false
+     */
+    public boolean compareVolume(Volume v1, Volume v2){
+        return  Double.compare(v1.value*v1.unit.baseUnitConversion, v2.value*v2.unit.baseUnitConversion) == 0;
     }
 }
--- src/main/java/com/bl/model/Volume.java
+++ src/main/java/com/bl/model/Volume.java
@@ -1,5 +1,5 @@
 /*******************************************************************************************
- * @Purpose: calculate quantity measurement
+ * @Purpose: calculate Volume in quantity measurement
  * @author: Tushar W.
  * @date: 18-05-2020
  *******************************************************************************************/
@@ -7,34 +7,35 @@ package com.bl.model;
 
 import com.bl.enums.Unit;
 import com.bl.exception.QuantityMeasurementException;
+
 import java.util.Objects;
 
-public class Length {
+public class Volume {
     /**
      * value variable store the number entered by the user as the input
-     * unit variable store the enum constanats
+     * unit variable store the enum constants
      */
-    public final int value;
+    public final double value;
     public Unit unit;
     /**
      * Parameterized constructor to take one input value
      * @param unit, value
      */
-    public Length(Unit unit, Integer value) throws QuantityMeasurementException {
+    public Volume(Unit unit, Double value) throws QuantityMeasurementException {
         try{
             this.unit = unit;
             this.value = value;
         }catch (NullPointerException e) {
-                throw new QuantityMeasurementException(QuantityMeasurementException.ExceptionType.NULL_VALUE, "VALUES IS NULL");
+            throw new QuantityMeasurementException(QuantityMeasurementException.ExceptionType.NULL_VALUE, "VALUES IS NULL");
         }
     }
     /**
-     * This method comapre unit length of the current object with object of same or different type
+     * This method comapre unit volume of the current object with object of same or different type
      * @param that
      * @return true or false
      */
-    public boolean compare(Length that) {
-        return  unit.compareLength(this, that);
+    public boolean compare(Volume that) {
+        return  unit.compareVolume(this,that);
     }
     /**
      * This method compares the equality of the current object with the object of same type
@@ -45,20 +46,16 @@ public class Length {
     public boolean equals(Object that) {
         if (this == that) return true;
         if (that == null || getClass() != that.getClass()) return false;
-        Length length = (Length) that;
-        if (value < 0 && length.value < 0)
+        Volume volume = (Volume) that;
+        if (value < 0 && volume.value < 0)
             throw new QuantityMeasurementException(QuantityMeasurementException.ExceptionType.NEGATIVE_VALUE,
-                                                          "VALUE_IS_NEGATIVE");
-        return value == length.value &&
-                unit == length.unit;
+                    "VALUE_IS_NEGATIVE");
+        return Double.compare(volume.value, value) == 0 &&
+                unit == volume.unit;
     }
 
     @Override
     public int hashCode() {
         return Objects.hash(value, unit);
-    }
-
-    public double add(Length that) {
-        return unit.add(this,that);
     }
 }
